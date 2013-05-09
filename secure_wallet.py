f967bd373f0f62d0083e8d9c8cd4776e1bbc0210
Amir Taaki <genjix@riseup.net>
2013-05-09 02:47:07 +0100

    use "if __name__ == "__main__"" so module can be imported into other code.

--- secure_wallet.py
+++ secure_wallet.py
@@ -187,8 +187,9 @@ class Application:
     def balance_changed(self, address, balances):
         print address, balances
 
-mpk = "3315ae236373067ea27d92f10f9475b1ff727eebe45f4ce4dd21cf548a237755397548d57fdb94610aef20993b4ff4695cae581d3be98743593336b21090c7d2".decode("hex")
-wallet = Wallet(mpk)
-app = Application(wallet)
-app.start()
+if __name__ == "__main__":
+    mpk = "3315ae236373067ea27d92f10f9475b1ff727eebe45f4ce4dd21cf548a237755397548d57fdb94610aef20993b4ff4695cae581d3be98743593336b21090c7d2".decode("hex")
+    wallet = Wallet(mpk)
+    app = Application(wallet)
+    app.start()
 
